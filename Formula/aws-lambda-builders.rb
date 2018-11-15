cedd95ef6f7ec42ad2f6f0de1c83b20045736407
Sanath Kumar Ramesh <sanathkr@amazon.com>
2018-11-15 10:09:58 -0800

    Create aws-lambda-builders.rb

---
+++ Formula/aws-lambda-builders.rb
@@ -0,0 +1,23 @@
+class AwsSamCli < Formula
+  include Language::Python::Virtualenv
+
+  desc "AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications"
+  
+  homepage "https://github.com/awslabs/aws-lambda-builders/"
+  head "https://github.com/awslabs/aws-lambda-builders.git", :branch => "develop"
+  bottle :unneeded
+
+  depends_on "python"
+
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", "--no-binary", ":all:",
+                              "--ignore-installed", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "aws-lambda-builders"
+    venv.pip_install_and_link buildpath
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/lambda-builders --help")
+  end
+end
